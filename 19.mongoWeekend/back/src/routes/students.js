c10da9e1a296a7881150ccac24b96a483e4bb723
shaharKamay <shahar.kamay@gmail.com>
2021-11-12 18:54:21 +0200

    created get all stude…

--- 19.mongoWeekend/back/src/routes/students.js
+++ 19.mongoWeekend/back/src/routes/students.js
@@ -5,6 +5,8 @@ const Student = require('../mongoDb/models/studentModel');
 
 const studentsRouter = express.Router();
 
+mongoClient.init();
+
 studentsRouter.post('/', async (req, res, next) => {
     const student = req.body.student;
     const { name, surname, phone, gender, courses } = student;
@@ -18,9 +20,7 @@ studentsRouter.post('/', async (req, res, next) => {
         && gender 
         && courses) {
             try {
-                mongoClient.init();
                 await new Student({ name, surname, birth, phone, gender, courses }).save();
-                mongoClient.close();
                 res.json('Student added successfully!')
                 res.end();
             } catch (error) {
@@ -34,9 +34,7 @@ studentsRouter.post('/', async (req, res, next) => {
 
 studentsRouter.get('/', async (req, res, next) => {
     try {
-        mongoClient.init();
         const student = await Student.find({});
-        mongoClient.close();
         res.json(student)
         res.end();
     } catch (error) {
@@ -49,9 +47,7 @@ studentsRouter.get('/name/:name', async (req, res, next) => {
     const name = req.params.name;
     if(name) {
         try {
-            mongoClient.init();
             const student = await Student.find({ name });
-            mongoClient.close();
             res.json(student)
             res.end();
         } catch (error) {
@@ -78,5 +74,47 @@ studentsRouter.get('/courses/:course', async (req, res, next) => {
         next({ status: 401, message: 'Bad request!' })
     }
 })
+
+studentsRouter.get('/courses', async (req, res, next) => {
+    const course = req.body.course;
+    const gender = req.body.gender;
+    if(course) {
+        try {
+            const student = await Student.find({ $and: [ { courses: { $in: course }, gender } ] });
+            res.json(student)
+            res.end();
+        } catch (error) {
+            console.log(error);
+            next({ status: 502, message: 'Bad Geteway!' });
+        }
+    } else {
+        next({ status: 401, message: 'Bad request!' })
+    }
+})
+
+studentsRouter.get('/birth', async (req, res, next) => {
+    const compareDate = moment('01/01/1998', "DD/MM/YYYY").toDate();
+    try {
+        const student = await Student.find({ birth: { $gt: compareDate } });
+        res.json(student)
+        res.end();
+    } catch (error) {
+        console.log(error);
+        next({ status: 502, message: 'Bad Geteway!' });
+    }
+})
+
+studentsRouter.get('/phone', async (req, res, next) => {
+    const phoneCompare = '054';
+    try {
+        const student = await Student.find({ phone: { $regex: new RegExp(`^${phoneCompare}`) } });
+        res.json(student)
+        res.end();
+    } catch (error) {
+        console.log(error);
+        next({ status: 502, message: 'Bad Geteway!' });
+    }
+})
+
 
 module.exports = { studentsRouter };
